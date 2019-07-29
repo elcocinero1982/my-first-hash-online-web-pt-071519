8b57bf4f9deb0f8225d7099817f4f0a012f5452e
jose luis <cortezluisjose@gmail.com>
2019-07-29 04:34:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,7 +38,7 @@ def adding
 shipping_manifest["2 muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["4 gun powder"] = "4 gun powder"
+shipping_manifest["4 gun powder"] = 4
 
 
   # return the shipping_manifest hash below
