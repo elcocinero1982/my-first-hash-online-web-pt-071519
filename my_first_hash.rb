71e35548e035df52b282b4df658e3e1ef1b28a6a
jose luis <cortezluisjose@gmail.com>
2019-07-29 04:27:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def retrieval
                   "oil paintings" => 3
                  }
                  
-sh
+shipping_manifest[""]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
